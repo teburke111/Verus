63e116e0064ced4308565df02b47c92d89ec66ba
Thomas Burke <teburke@head.teburke-272834.cloud-edu.emulab.net>
2025-10-14 14:13:02 -0600

    image process backend

--- backend/image/process/app.py
+++ backend/image/process/app.py
@@ -2,20 +2,18 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024  # 50 MB
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return "Backend is from image!"
-
-@app.route('/image-process', methods=['POST'])
-def process_image():
-    if 'image' not in request.files:
-        return jsonify({'message': 'No image file provided'}), 400
-
-    image = request.files['image']
-    filename = image.filename
-    print(f"Received image: {filename}")
-
-    return jsonify({'message': f"Image '{filename}' was received successfully!"})
+    if request.method == 'POST':
+        if 'image' not in request.files:
+            return jsonify({'message': 'No image file provided'}), 400
+        image = request.files['image']
+        filename = image.filename
+        return jsonify({'message': f"Image '{filename}' was received successfully!"})
+    else:
+        return "Backend is from image!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
