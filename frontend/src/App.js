235f5cc2a796f19664765f48176f939a764da8b6
Isreal210 <90050028+Isreal210@users.noreply.github.com>
2025-11-06 14:45:25 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,28 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
-import { Navigate } from "react-router-dom";
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Signup from './pages/Signup';
 import Images from './pages/Images';
 import Text from './pages/Text';
 import Audio from './pages/Audio';
 import Video from './pages/Video';
 import MyUploads from "./pages/MyUploads";
 
-
 function App() {
   return (
     <div className="App">
-      {/* <Login /> */}
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/Images" element={<Images />} />
         <Route path="/Text" element={<Text />} />
         <Route path="/Audio" element={<Audio />} />
         <Route path="/Video" element={<Video />} />
         <Route path="/my-uploads" element={<MyUploads />} />
-        <Route path="*" element={<Navigate to="/" />} />
-  
       </Routes>
     </div>
-    
   );
 }
 
