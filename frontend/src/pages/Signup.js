7742494d08fe326fb82d6311ddfb8bfe864ae1fe
Isreal210 <90050028+Isreal210@users.noreply.github.com>
2025-11-06 22:21:28 -0500

    Create Signup.js

---
+++ frontend/src/pages/Signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SignupBox from "../components/SignupBox";
+
+function Signup() {
+  return (
+    <div className="signupPage">
+      <SignupBox />
+    </div>
+  );
+}
+
+export default Signup;
